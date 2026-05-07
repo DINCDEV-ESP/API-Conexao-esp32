a96cc774eca73cf2f52aa03fd69bde117102528b
Luiz Felipe Piva <luizfelipepiva@gmail.com>
2026-05-07 14:06:43 -0300

    Adicionando contador de bateria

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import enviarRoute from "./src/enviar.js";
 import { initReceberListener } from "./src/receber.js";
+import { initBateriaListener } from "./src/bateria.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,4 +18,5 @@ app.use("/enviar", enviarRoute);
 app.listen(port, "0.0.0.0", () => {
   console.log(`Servidor rodando na porta ${port}`);
   initReceberListener();
+  initBateriaListener();
 });
---
+++ src/bateria.js
@@ -0,0 +1,60 @@
+import mqtt from "mqtt";
+
+let bateriaAtual = 0;
+
+
+function initBateriaListener() {
+  const client = mqtt.connect("mqtt://broker.hivemq.com");
+
+  client.on("connect", () => {
+    console.log("Conectado ao broker MQTT");
+
+    client.subscribe("dinc/paciente/bateria", (err) => {
+      if (err) {
+        console.log("Erro ao se inscrever:", err);
+        return;
+      }
+
+      console.log("Inscrito no tópico dinc/paciente/bateria");
+    });
+  });
+
+  client.on("message", (topic, message) => {
+    if (topic !== "dinc/paciente/bateria") return;
+
+    try {
+      const data = JSON.parse(message.toString());
+
+      console.log("Mensagem recebida:");
+      console.log(data);
+
+      var tensao = Number(data.voltagem);
+      var porcentagem = Math.round(((tensao - 7) / 5) * 100);
+      porcentagem = Math.max(0, Math.min(100, porcentagem));
+
+      console.log("Bateria: " + porcentagem + "%");
+
+      console.log("Nível da bateria:", porcentagem + "%");
+
+      bateriaAtual = porcentagem;
+    } catch (err) {
+      console.log("Erro ao converter JSON:", err.message);
+
+      // caso venha apenas texto simples
+      // console.log("Mensagem bruta:", message.toString());
+    }
+  });
+
+  client.on("error", (err) => {
+    console.log("Erro MQTT:", err.message);
+  });
+}
+
+app.get("/bateria", (req, res) => {
+  res.json({
+    bateria: bateriaAtual
+  });
+});
+
+
+export { initBateriaListener };
